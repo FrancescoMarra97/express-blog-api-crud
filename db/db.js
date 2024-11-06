d411801f90df853504135dcdcc02ec7983910fd1
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-06 16:17:04 +0100

    test on postman

--- db/db.js
+++ db/db.js
@@ -33,9 +33,9 @@ module.exports = [
         ]
     },
     {
-        "title": "Pane fritto dolce",
+        "title": "Pane fritto dolce 2",
         "slug": "pane-fritto-dolce",
-        "content": "Il pane fritto dolce è la versione più antica dell’odierno french toast! Una deliziosa ricetta antispreco che le nonne preparavano ai bambini per merenda, utilizzando gli ingredienti che si avevano sempre a disposizione in casa: pane raffermo, uova, latte e zucchero, che noi abbiamo deciso di aromatizzare con un pizzico di cannella. Facile e veloce da realizzare, il pane fritto dolce vi riporterà con la mente ai sapori dell’infanzia… gustatelo da solo o accompagnatelo con frutta fresca e yogurt per uno spuntino tanto goloso quanto genuino!",
+        "content": "pane dolce",
         "image": "pane_fritto_dolce.jpeg",
         "tags": [
             "Dolci",
