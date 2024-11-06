340dc649c36d39f9bb5d5689521c84eaac6adb10
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-06 15:59:25 +0100

    fix bug

--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,5 @@ const postsControllers = require('../controllers/postControllers.js')
 router.get('/:slug', postsControllers.show)
 router.get('/', postsControllers.index)
 router.post('/', postsControllers.store)
-router.put("/:slug". postsControllers.update)
+router.put("/:slug", postsControllers.update)
 module.exports = router
