a8d4c0ef78f19e1910591b27187ec79ecc3fefe8
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-07 15:46:58 +0100

    add loggermiddleware in app

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const postsRouter = require('./routes/posts.js')
 const notFoundMiddleware = require("./middlewares/notFound.js")
+const loggerMiddleware = require('./middlewares/loggerMiddleware')
 const PORT = process.env.PORT
 const HOST = process.env.HOST
 
+app.use('/posts', loggerMiddleware)
 app.use(express.json())
 app.use(express.static('public'))
-
+app.use('/posts', (req, res, next) => {
+	throw new Error("You broke everything dude! 💥");
+  }); 
 app.use('/posts', postsRouter)
 
 app.listen(PORT, (req, res) => {
