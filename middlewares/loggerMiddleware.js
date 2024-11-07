7bde21bec385cc6581adb2c8657d417b565a9988
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-07 15:44:10 +0100

    loggerMiddleware

--- middlewares/loggerMiddleware.js
+++ middlewares/loggerMiddleware.js
@@ -1,1 +1,12 @@
-const loggerMiddleware = ()
+const loggerMiddleware = (req, res, next) => {
+    const now = new Date().toString();
+    console.error(`
+        Date: ${now} 
+    Method: ${req.method} 
+    URL: ${req.url}
+        `);
+
+    next()
+}
+
+module.exports = loggerMiddleware;
