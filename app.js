6d31e1f13224aa93fb286ffee81d7fdedc2e27c4
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-07 15:41:59 +0100

    add the middleware in app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
-
-app.use(express.json())
-
-
+const postsRouter = require('./routes/posts.js')
+const notFoundMiddleware = require("./middlewares/notFound.js")
 const PORT = process.env.PORT
 const HOST = process.env.HOST
-app.use(express.static('public'))
 
-const postsRouter = require('./routes/posts.js')
+app.use(express.json())
+app.use(express.static('public'))
 
 app.use('/posts', postsRouter)
 
 app.listen(PORT, (req, res) => {
 	console.log(`Server is running in ${HOST}:${PORT}`)
 })
+app.use(notFoundMiddleware)
