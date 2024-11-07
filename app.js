19b4437980c5d9aaf3190d8a0a4258524de9db76
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-07 15:48:07 +0100

    last middleware

--- app.js
+++ app.js
@@ -18,3 +18,12 @@ app.listen(PORT, (req, res) => {
 	console.log(`Server is running in ${HOST}:${PORT}`)
 })
 app.use(notFoundMiddleware)
+app.use((err, req, res, next) => {
+	console.log("Error: ", err.message);
+	console.error(err.stack);
+	res.status(500).send({
+	  message: "Something went wrong",
+	  error: err.message
+	})
+  });
+  
